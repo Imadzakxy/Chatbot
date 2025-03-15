38048b5ab41cfba94185b1c486014e1638c2848f
Imadzakxy <imadzakxy@gmail.com>
2025-03-15 20:16:44 +0100

    log_in

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
   //   );
   return (
     <>
-      <h1>Chatbot</h1>
       <Log_in />
     </>
   );
--- src/components/Log_in.tsx
+++ src/components/Log_in.tsx
@@ -1,42 +1,63 @@
 import './Log_in.css'
-
+import React, { useEffect } from "react";
 
 function Log_in() {
-    // let user,pass;
-    // const conection=()=>{
-    //     user!=null && pass!=null ? window.location.href='./acce.html' : console.log("error")
-    // }
+    useEffect(() => {
+        const starsContainer = document.querySelector(".njmat");
+
+        if (starsContainer) {
+            for (let i = 0; i < 43; i++) { // Génère 50 étoiles
+                const star = document.createElement("div");
+                star.className = `star ${Math.random() > 0.7 ? "small" : "tiny"}`;
+                star.style.left = Math.random() * 100 + "vw"; 
+                star.style.top = Math.random() * 30 + "vh";
+                starsContainer.appendChild(star);
+            }
+        }
+    }, []);
     return (
-        <>
-            <div className="form-b">hi</div>
-                {/* <div class="inputbox">
-                    <input type="Username" placeholder="Username" required>
-                    {user=""}
-                    <div class="bx bx-user"></div>
-                </div>
-                <div class="inputbox">
-                    <input type="password" placeholder="password" required>
-                    {pass=""}
-                    <div class="bx bx-lock-alt"></div>
-                </div>
-                <div class="remember">
-                <label for=""><input type="checkbox">remember me</label>
-                {
-                    // localStorage.setItem('user')
-                    // localStorage.setItem('pass')
-                }
-                <a href="./forgot_pss.html">forgot password!</a>
+        <>  
+            <div className="njmat">
             </div>
-            <button type="submit" class="btn" onclick="window.location.href='./acce.html'">log in</button>
-            {conection}
-            <div class="register">
-                <p>Don't have an account ? <a href = "./inscription.html">sing up</a></p>
+
+            <header>
+                <h1>Chatbot</h1>
+            </header>
+            
+            <div className="box">
+
+                <div className="inputbox">
+                    <input type="Username" placeholder="Username" required /><div className="user"></div>
+                </div>
+
+                <div className="inputbox">
+                    <input type="Password" placeholder="Password" required /><div className="lock"></div>
+                </div>
+
+                <div className="remember">
+                    <span className="rem">
+                        <input type="checkbox" className="check"/>
+                        Remember me
+                    </span>
+                    <button className="forgot">Forgot password!</button>
+                </div>
+
+                <div className="login">
+                    <button type="submit" className="btn" onClick={() => window.location.href='./Chat.tsx'}>log in</button>
+                </div>
+
+                <div className="register">
+                    <span className="creat">Don't have an account ? 
+                        <button className="signup">sign up!</button>
+                        {/* onClick={() => window.location.href='./Sign_up.tsx'} */}
+                    </span>
+                </div>
             </div>
-            <div class="help">
-                <p>
-                <div class='bx-fw bx bxl-instagram'></div>if you have problem
-                    <a href="https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fimad.zak.xy%2F&is_from_rle">contact us!</a>
-                </p> */}
+            <div className="help">
+                    <span className='contact'>if you have problem
+                        <a className='acc' href="https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fimad.zak.xy%2F&is_from_rle">contact us!</a>
+                    </span>
+            </div>
         </>
     );
 }
